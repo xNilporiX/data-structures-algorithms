d7a19a600148293ff8ef9c9442746dfab69534b7
Aninda Dhar <aninda.dhar@chamonix.com.au>
2025-09-24 12:18:27 +0930

    flatten works!

--- src/divide-and-conquer/findRotatedIndex.js
+++ src/divide-and-conquer/findRotatedIndex.js
@@ -5,29 +5,44 @@
  * @param {*} target
  */
 function findRotatedIndex(arr, target) {
+  if (!arr.length) return -1;
   let left = 0;
   let right = arr.length - 1;
 
   while (left <= right) {
-    var mid = Math.floor((left + right) / 2);
-    console.log("mid", mid, arr[mid]);
+    let mid = Math.floor((left + right) / 2);
+
+    // If the middle element of the array matches, just return index.
     if (arr[mid] === target) {
       return mid;
-    } else if (arr[mid] > target) {
-      left = mid+1;
-      console.log("Left ->", left, '', arr[left]);
+    }
+
+    // If array at the left side is less than the mid.
+    if (arr[left] <= arr[mid]) {
+      if (arr[left] <= target && target < arr[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+      // Otherwise if array at left is more than the mid, we look at the right portion
     } else {
-      right = mid-1;
-      console.log("Right <-", right, arr[right]);
+      // If array at mid is less than the target and the target is less than arr of right
+      if (arr[mid] < target && target <= arr[right]) {
+        left = mid + 1;
+      }
+      // Otherwise decrease from the right
+      else {
+        right = mid - 1;
+      }
     }
   }
 
   return -1;
 }
 
-// console.log(findRotatedIndex([3,4,1,2],4)) // 1
-// console.log(findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 8)) // 2
+console.log(findRotatedIndex([3, 4, 1, 2], 4)); // 1
+console.log(findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 8)); // 2
 console.log(findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 3)); // 6
-// console.log(findRotatedIndex([37,44,66,102,10,22],14)) // -1
-// console.log(findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 12)) // -1
-// console.log(findRotatedIndex([11,12,13,14,15,16,3,5,7,9], 16)) // 5
+console.log(findRotatedIndex([37, 44, 66, 102, 10, 22], 14)); // -1
+console.log(findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 12)); // -1
+console.log(findRotatedIndex([11, 12, 13, 14, 15, 16, 3, 5, 7, 9], 16)); // 5
--- src/recursion/flatten.js
+++ src/recursion/flatten.js
@@ -6,19 +6,30 @@ function flatten(arrOfArrs) {
   //  The idea is: We have an array of arrays
   //  We will recursively go through the outermost array.
   //  As long as its not an array within the array, we store that value,
-  //  Otherwise, we will keep popping that array until the element itself is a number.
+  //  Otherwise, we will keep recursively calling that array until the element itself is a number.
   if (arrOfArrs.length === 0) return arrOfArrs;
+  let flattenedElement = [];
 
-  function helper(arrOfArrs) {
-    if (isNaN(arrOfArrs[0])) {
-      arrOfArrs.pop();
-    }
+  //  using [1, 2, 3, [4, 5]] as an example. 
+  //  Checks if the Element itself is an array.
+  //  We reach a stage where the last element would be the nested array [[4,5]] after we sliced through it.
+  if (Array.isArray(arrOfArrs[0])) {
+    //  Now that we know [[4,5]] is an array, we call the recursive function again.
+    //  But this time we just pass in the array element
+    //  [4, 5] -> (not nested anymore), thus skips this if condition and goes to else 
+    flattenedElement = flatten(arrOfArrs[0]);
+  } else {
+    // If there is no nested array, store the element
+    // For instance we store [1, 2, 3] without recursively flattening it.
+    flattenedElement = [arrOfArrs[0]];
   }
 
-  return flatten(arrOfArrs.slice(1));
+  //  Concatenate the element and slice it.
+  //  First iteration should look like this after slicing : [2,3, [4, 5]] and so on. 
+  return flattenedElement.concat(flatten(arrOfArrs.slice(1)));
 }
 
 console.log(flatten([1, 2, 3, [4, 5]])); // [1, 2, 3, 4, 5]
-console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
-console.log(flatten([[1],[2],[3]])) // [1,2,3]
-console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3]
+console.log(flatten([1, [2, [3, 4], [[5]]]])); // [1, 2, 3, 4, 5]
+console.log(flatten([[1], [2], [3]])); // [1,2,3]
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])); // [1,2,3]
